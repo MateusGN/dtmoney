fbc2eb2982026f1fba42b8ee4bd0438eed62a07d
MateusKha <mateusgnascimento@outlook.com>
2022-01-25 15:54:49 -0300

    Styled Components and Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  return <div>asda</div>;
+  return (
+    <div>
+      <GlobalStyle />
+      Heloo
+    </div>
+  );
 }
